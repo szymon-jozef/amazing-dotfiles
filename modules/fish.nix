0afa633a488c8f3c8f3de025a46aaf8e6c2f4eab
Szymon P <szymon_jozef@proton.me>
2026-02-20 13:04:57 +0100

    update: modules/fish.nix

--- modules/fish.nix
+++ modules/fish.nix
@@ -11,17 +11,10 @@
       # no dum greeting
       set fish_greeting ""
 
-      # yes, please setup git for me
-      gh auth setup-git
-
-      # add github gpg key
       set -gx GPG_TTY (tty)
 
-      # vi mode
       fish_vi_key_bindings
 
-      # Odpalenie fastfetcha na start
-      clear
       fastfetch --config ~/.config/fastfetch/startup.jsonc
     '';
 
