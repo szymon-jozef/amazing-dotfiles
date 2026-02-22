f9746b4d810d8b43a595f02535cc83a3244ee912
Szymon P <szymon_jozef@proton.me>
2026-02-22 20:52:12 +0100

    xddddxdx

--- scripts/old/brightness.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-if [[ $1 == "up" ]]; then
-    ddcutil -d 1 setvcp 10 100 && ddcutil -d 2 setvcp 10 100
-elif [[ $1 == "down" ]]; then
-    ddcutil -d 1 setvcp 10 30 && ddcutil -d 2 setvcp 10 0
-else
-    echo "Usage: brightness.sh [up/down]"
-fi
--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,4 +1,4 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 # variables
 ## path vars
