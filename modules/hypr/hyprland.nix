96e33b9bc45d881c0058146640e9ab1ba7d9c303
Szymon P <szymon_jozef@proton.me>
2026-02-28 09:07:16 +0100

    update: modules/hypr/hyprland.nix

--- modules/hypr/hyprland.nix
+++ modules/hypr/hyprland.nix
@@ -296,10 +296,10 @@ in
       binde = [
         "$mainMod alt, equal, exec, hyprctl -q keyword cursor:zoom_factor $(hyprctl getoption cursor:zoom_factor | awk '/^float.*/ {print $2 * 1.1}')"
         "$mainMod alt, minus, exec, hyprctl -q keyword cursor:zoom_factor $(hyprctl getoption cursor:zoom_factor | awk '/^float.*/ {print $2 * 0.9}')"
-        "$mainMod L_SHIFT, l, colresize, 10 0"
-        "$mainMod L_SHIFT, h, colresize, -10 0"
-        "$mainMod L_SHIFT, k, colresize, 0 -10"
-        "$mainMod L_SHIFT, j, colresize, 0 10"
+        "$mainMod L_SHIFT, l, resizeactive, 10 0"
+        "$mainMod L_SHIFT, h, resizeactive, -10 0"
+        "$mainMod L_SHIFT, k, resizeactive, 0 -10"
+        "$mainMod L_SHIFT, j, resizeactive, 0 10"
       ];
 
       bindel = [
