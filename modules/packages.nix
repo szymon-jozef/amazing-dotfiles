101ae8da40c3a68fb94ce7f598a91002bf9920d6
Szymon P <szymon_jozef@proton.me>
2026-04-28 19:12:14 +0200

    feat: add more apps

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,4 +1,10 @@
-{ pkgs, pkgs-stable, ... }:
+{
+  pkgs,
+  pkgs-stable,
+  lib,
+  userConfig,
+  ...
+}:
 
 let
   rstudio-with-packages = pkgs-stable.rstudioWrapper.override {
@@ -10,40 +16,50 @@ let
   };
 in
 {
-  home.packages = with pkgs; [
-    # Cli stuff
-    libnotify
-    cliphist
-    gh
-    trash-cli
-    pandoc
-    fzf
-    eza
-    fastfetch
-    fd
-    duf
-    gdu
-    jq
-    tealdeer
-    grim
-    slurp
-    bc
-    nh
-    runc
+  home.packages =
+    with pkgs;
+    [
+      # Cli stuff
+      libnotify
+      cliphist
+      gh
+      trash-cli
+      pandoc
+      fzf
+      eza
+      fastfetch
+      fd
+      duf
+      gdu
+      jq
+      tealdeer
+      grim
+      slurp
+      bc
+      nh
+      runc
 
-    # Music
-    spotify
+      # Music
+      spotify
 
-    # Gui stuff
-    homebank
-    gimp
-    rstudio-with-packages
+      # Gui stuff
+      homebank
+      gimp
+      rstudio-with-packages
 
-    # Messaging
-    vesktop
-    signal-desktop
+      # Messaging
+      vesktop
+      signal-desktop
 
-  ];
+    ]
+    ++ lib.optionals userConfig.gaming [
+      mangohud
+      prismlauncher
+      heroic
+      # lutris
+      rpcs3
+      pcsx2
+    ];
 
   xdg.configFile."containers/containers.conf".text = ''
     [engine]
